1effd2cbbfbca3a19cb28059753b825b0c5c250e
takumi314 <isospin_symmetry@me.com>
2017-10-18 01:47:23 +0900

    create new NetworkEngine

---
+++ Sample-Networking-with-URLSession/Networking/NetworkEngine.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkEngine.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/18.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
