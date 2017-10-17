6d514de1f29a54d3f2d0ae847fce597590c9caf8
takumi314 <isospin_symmetry@me.com>
2017-10-18 02:14:51 +0900

    create new Downloader

---
+++ Sample-Networking-with-URLSession/Networking/Downloader.swift
@@ -0,0 +1,9 @@
+//
+//  Downloader.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/18.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
