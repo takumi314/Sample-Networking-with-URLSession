502931592d7e603cf09f651d5de7b665cd7b6e0b
takumi314 <isospin_symmetry@me.com>
2017-10-19 23:41:39 +0900

    create new DataLoader

---
+++ Sample-Networking-with-URLSession/Networking/DataLoader.swift
@@ -0,0 +1,9 @@
+//
+//  DataLoader.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/19.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
