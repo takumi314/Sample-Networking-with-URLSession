46251a0f93aec61e393fd2a15e78498b428c9c15
takumi314 <isospin_symmetry@me.com>
2017-10-23 02:26:03 +0900

    implement properties of object created by Downloader

--- Sample-Networking-with-URLSession/Models/Download.swift
+++ Sample-Networking-with-URLSession/Models/Download.swift
@@ -10,4 +10,16 @@ import Foundation
 
 struct Download {
 
+    var track: Track
+    init(track: Track) {
+        self.track = track
+    }
+
+    // provided by Downloader
+    var task: URLSessionDownloadTask?
+    var isDownloading = false
+    var resumeData: Data?
+
+    // provided by Downloader delegate
+    var progress: Float = 0
 }
