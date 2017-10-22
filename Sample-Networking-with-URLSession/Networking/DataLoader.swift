d0483f6610cf0e2d1503e3bc75b57087a6b90c56
takumi314 <isospin_symmetry@me.com>
2017-10-23 01:32:20 +0900

    make new class as DataLoader

--- Sample-Networking-with-URLSession/Networking/DataLoader.swift
+++ Sample-Networking-with-URLSession/Networking/DataLoader.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class DataLoader {
+
+    enum Result: Equatable {
+        case data(Data)
+        case error(Error)
+
+        static func ==(lhs: Result, rhs: Result) -> Bool {
+            switch (lhs, rhs) {
+            case (.data, .data):
+                return true
+            case (.error, .error):
+                return true
+            default:
+                return false
+            }
+        }
+    }
+
+    private let engine: NetworkEngine
+
+    init(_ engine: NetworkEngine) {
+        self.engine = engine
+    }
+
+    func load(from url: URL, completionHadler: @escaping (Result) -> Void) {
+        engine.performRequest(for: url) { (data, response, error) in
+            if let error = error {
+                return completionHadler(.error(error))
+            }
+            completionHadler(.data(data ?? Data()))
+        }
+    }
+
+
+}
