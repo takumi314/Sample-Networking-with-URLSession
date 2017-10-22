c1b460945c2c147d7df3bc71268f379c5fa90165
takumi314 <isospin_symmetry@me.com>
2017-10-23 01:45:22 +0900

    create new model as Track

---
+++ Sample-Networking-with-URLSession/Models/Track.swift
@@ -0,0 +1,13 @@
+//
+//  Track.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/23.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    
+}
