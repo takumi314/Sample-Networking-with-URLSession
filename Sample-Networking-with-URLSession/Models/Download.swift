a760118f4bbad5d2f642be9bae6fd8862b291a4a
takumi314 <isospin_symmetry@me.com>
2017-10-23 01:44:23 +0900

    create new model as Download

---
+++ Sample-Networking-with-URLSession/Models/Download.swift
@@ -0,0 +1,13 @@
+//
+//  Download.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/23.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+struct Download {
+
+}
