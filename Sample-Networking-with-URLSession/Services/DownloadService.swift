cbcb2b6b8c293de8f74bfd502896de7665505b4c
takumi314 <isospin_symmetry@me.com>
2017-10-24 00:48:52 +0900

    create new class as DownloadService

---
+++ Sample-Networking-with-URLSession/Services/DownloadService.swift
@@ -0,0 +1,13 @@
+//
+//  DownloadService.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/24.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+class DownloadService {
+    
+}
