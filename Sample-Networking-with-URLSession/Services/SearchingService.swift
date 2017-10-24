39485f2e97885dc1791360a59955baed35adccde
takumi314 <isospin_symmetry@me.com>
2017-10-25 01:53:44 +0900

    create new class as SearchingService

---
+++ Sample-Networking-with-URLSession/Services/SearchingService.swift
@@ -0,0 +1,13 @@
+//
+//  SearchingService.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/25.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+class SearchingService {
+    
+}
