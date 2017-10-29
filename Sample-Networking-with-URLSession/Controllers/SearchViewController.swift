fecc7473bb2c06d5b4fb0871ce91bcaa85f0de4b
takumi314 <isospin_symmetry@me.com>
2017-10-29 17:39:40 +0900

    set UITableView and UISearchBar on the storyboard

--- Sample-Networking-with-URLSession/Controllers/SearchViewController.swift
+++ Sample-Networking-with-URLSession/Controllers/SearchViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    private var searchResults = [Track]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,57 @@ class SearchViewController: UIViewController {
     }
 
 
+}
+
+extension SearchViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trackCell", for: indexPath)
+
+        return cell
+    }
+
+}
+
+extension SearchViewController: UITableViewDelegate {
+
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        if !searchBar.isFirstResponder {
+            searchBar.becomeFirstResponder()
+        }
+        return true
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Execution
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // Cancel
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Validation
+    }
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        return true
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+
+    }
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        if searchBar.isFirstResponder {
+            searchBar.resignFirstResponder()
+        }
+        return false
+    }
 }
 
