34b44f32bb3deaff61862dd140f64811a8ee2f86
takumi314 <isospin_symmetry@me.com>
2017-10-29 17:42:43 +0900

    create new Cell class

---
+++ Sample-Networking-with-URLSession/Views/TrackCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackCell.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/10/29.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+}
