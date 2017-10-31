9ebb1cd56cf76d7c3c9bd94942614ec72ead0442
takumi314 <isospin_symmetry@me.com>
2017-11-01 00:30:57 +0900

    create new class as DownloadTaskStore

---
+++ Sample-Networking-with-URLSession/Library/DownloadTaskStore.swift
@@ -0,0 +1,19 @@
+//
+//  DownloadTaskStore.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/11/01.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+class DownloadTaskStore {
+
+    private let manager: FileManager
+
+    init(_ manager: FileManager) {
+        self.manager = manager
+    }
+
+}
