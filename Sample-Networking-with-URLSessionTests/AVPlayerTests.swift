d95d918e2d3fbc0affbab15757535b342f5f0290
takumi314 <isospin_symmetry@me.com>
2017-11-03 02:29:48 +0900

    update XCTestCase

--- Sample-Networking-with-URLSessionTests/AVPlayerTests.swift
+++ Sample-Networking-with-URLSessionTests/AVPlayerTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import AVKit
+@testable import Sample_Networking_with_URLSession
 
 class AVPlayerTests: XCTestCase {
     
@@ -24,7 +26,20 @@ class AVPlayerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
+    func testPlaye() {
+        let path = "file:///Users/takumi/Library/Developer/CoreSimulator/Devices/B43DD9B3-D8C3-4E40-AA5F-ECC4F5491C75/data/Containers/Data/Application/37DB8880-F43A-4903-B230-F99F4807B449/tmp/CFNetworkDownload_YkC7NL.tmp"
+        let url = URL(fileURLWithPath: path)
+        let track = Track(name: "", artist: "", previewURL: url, index: 0)
+
+        let parent = UIViewController()
+        let service = AVPlayingService(track: track, parent: parent)
+
+
+        // targe ?
+
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
