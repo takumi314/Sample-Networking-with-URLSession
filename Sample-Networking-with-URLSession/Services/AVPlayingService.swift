4900160298ebefb6ce0941249f4d0da758c3d76c
takumi314 <isospin_symmetry@me.com>
2017-11-03 01:24:08 +0900

    create new class as AVPlayingService

---
+++ Sample-Networking-with-URLSession/Services/AVPlayingService.swift
@@ -0,0 +1,14 @@
+//
+//  AVPlayingService.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/11/03.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+class AVPlayingService: NSObject {
+
+}
