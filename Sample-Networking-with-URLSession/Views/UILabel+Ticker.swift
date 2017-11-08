32537416e873ee0c552b2a76bcef4cfa8c9d7c38
takumi314 <isospin_symmetry@me.com>
2017-11-09 01:27:33 +0900

    extend UILabel

---
+++ Sample-Networking-with-URLSession/Views/UILabel+Ticker.swift
@@ -0,0 +1,17 @@
+//
+//  UILabel+Ticker.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/11/09.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+protocol Ticking {
+
+}
+
+extension UILabel {
+
+}
