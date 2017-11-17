833e3f9a4de07bc9d17273420390709287df5bb0
takumi314 <isospin_symmetry@me.com>
2017-11-18 01:41:46 +0900

    add new class inherited from UIScrollview

---
+++ Sample-Networking-with-URLSession/Views/TickerScrollview.swift
@@ -0,0 +1,13 @@
+//
+//  TickerScrollview.swift
+//  Sample-Networking-with-URLSession
+//
+//  Created by NishiokaKohei on 2017/11/18.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class TickerScrollview: UIScrollView {
+
+}
